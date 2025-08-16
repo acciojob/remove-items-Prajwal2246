ddc11d40d431f00da28481a6237d6242cd66a83a
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-08-16 12:28:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 const elem= document.getElementById("colorSelect");
 document.getElementById("deletebtn").addEventListener("click",()=>{
-	const selectedindex = elem.selectedindex;
-	if(selectedindex !== -1){
-		select.remove(selectindex);
+	const selectedIndex = elem.selectedIndex;
+	if(selectedIndex !== -1){
+		elem.remove(selectedIndex);
 	}
 })
